c788e0a430e673912ec40a18be7c6d0020fd4b41
Naveen Chilakala <nchilakala.c@lifevantage.com>
2025-08-25 17:15:13 -0400

    delete frontend folder

--- frontend/tailwind.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  // In Tailwind v4, configuration is handled via CSS @theme directive
-  // Content detection handled by @source in CSS
-}
